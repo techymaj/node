0984d7884a64fbc1d7dd85f897ace77d67fad50d
Wilfried M Majaliwa <wilfriedmajaliwa@gmail.com>
2024-07-07 01:32:42 +0300

    Pass By Value VS Pass By Reference

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
-function Person(lastname, firstname) {
-    this.lastname = lastname // this is an empty object when a new Person is created
-    this.firstname = firstname
+// pass by value
+
+function change(b) {
+    b = 2
 }
 
-// prototype of any object created from Person
-Person.prototype.greet = function() {
-    return `Heya ${this.lastname}`
+let a = 1;
+
+change(a)
+
+console.log(a)
+
+function changeObj(d) {
+    d.prop1 = function() {}
+    d.prop2 = {}
 }
 
-var wilfried = new Person('Majaliwa', 'Wilfried')
-// console.log(wilfried.lastname)
-console.log(wilfried.greet())
-
-var maria = new Person('Nansikombi', 'Maria')
-// console.log(wilfried.lastname)
-console.log(maria.greet())
-
-console.log(wilfried.__proto__)
-console.log(maria.__proto__)
-console.log(wilfried.__proto__ === maria.__proto__)
+let c = {}
+c.prop1 = {}
+changeObj(c)
+console.log(c)
