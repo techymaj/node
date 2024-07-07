7e99dfecc4d98244d8a97022b4e07a3146f92038
Wilfried M Majaliwa <wilfriedmajaliwa@gmail.com>
2024-07-07 05:39:09 +0300

    modules and es6

---
+++ app.mjs
@@ -0,0 +1,9 @@
+// const util = require('util');
+import * as hello from './greetings/es6.mjs';
+
+// const name = 'Majaliwa';
+// const greeting = util.format(`Hello, ${name}`);
+// util.log(greeting);
+
+hello.greeting('I am the future!')
+
---
+++ greetings/es6.mjs
@@ -0,0 +1,3 @@
+export function greeting(text) {
+    console.log(text);
+}
