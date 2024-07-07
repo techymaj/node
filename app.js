6dc663742d0a66fc6388867410db9c837af005f4
Wilfried M Majaliwa <wilfriedmajaliwa@gmail.com>
2024-07-07 16:20:25 +0300

    Inheriting from the Event Emitter Using ES6

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
 const EventEmitter = require('node:events');
-const util = require('node:util');
+// const util = require('node:util');
 
-function Greetr() {
-    this.greeting = 'Hello World!';
+class Greetr extends EventEmitter {
+    start(data) {
+        console.log(`Greetr started! ${data}`);
+        this.emit('ready', data);
+    }
 }
 
 // any objects created from Greetr should also have access
 // to the properties and methods on the prototype of EventEmitter
-util.inherits(Greetr, EventEmitter);
+// util.inherits(Greetr, EventEmitter);
 // inherits(childConstructor, superConstructor)
 
-Greetr.prototype.start = function (data) {
-    console.log(`Greetr started! ${data}`);
-    this.emit('ready', data);
-}
+// Greetr.prototype.start = function (data) {
+//     // console.log(`Greetr started! ${data}`);
+//     // this.emit('ready', data);
+// }
 
 const startGreetr = new Greetr();
 
