82a8c7b1e31282d7cfe508894b696b64a816f9b6
Wilfried M Majaliwa <wilfriedmajaliwa@gmail.com>
2024-07-08 21:29:34 +0300

    Express Routes

--- app.js
+++ app.js
@@ -7,6 +7,13 @@ app.get('/', (req, res) => {
     res.send('<html><body><h1>Hello World!</h1></body></html>');
 })
 
+app.get('/person/:id', (req, res) => {
+    console.log(req.body);
+    res.send(`
+    <html><body><h1>Person ${req.params.id}</h1></body></html>
+`);
+})
+
 app.get('/api', (req, res) => {
     res.json(
         {
