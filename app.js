86fd57125441f5bb4152025aac8e5a8d94a2f22a
Wilfried M Majaliwa <wilfriedmajaliwa@gmail.com>
2024-07-08 17:43:32 +0300

    Outputting JSON

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ const fs = require('node:fs');
 
 http.createServer((req, res) => {
 // executed when a request happens
-    res.writeHead(200, {'Content-Type': 'text/html'}); // when a req happens, do this, construct a response
-    const html = fs.createReadStream(`${__dirname}/index.htm`, 'utf8');
-    html.pipe(res);
+    res.writeHead(200, {'Content-Type': 'application/json'}); // when a req happens, do this, construct a response
+    const data = {
+        name: "Wilfried M. Majaliwa",
+        age: 29
+    }
+    res.end(JSON.stringify(data));  // serialize the data into JSON
 }).listen(8000, '127.0.0.1', null, function (){
     console.log('Server is running on port 8000');
 });
