ed8ffeebdeb150418a4dedabe7d5ce7aa89571e4
Wilfried M Majaliwa <wilfriedmajaliwa@gmail.com>
2024-07-08 17:20:14 +0300

    Streams & Performance. (Use streams whenever you can)

--- app.js
+++ app.js
@@ -4,10 +4,8 @@ const fs = require('node:fs');
 http.createServer((req, res) => {
 // executed when a request happens
     res.writeHead(200, {'Content-Type': 'text/html'}); // when a req happens, do this, construct a response
-    let html = fs.readFileSync(`${__dirname}/index.htm`, 'utf8');
-    const message = 'Wilfried M. Majaliwa';
-    html = html.replace('{Message}', message);
-    res.end(html);
+    const html = fs.createReadStream(`${__dirname}/index.htm`, 'utf8');
+    html.pipe(res);
 }).listen(8000, '127.0.0.1', null, function (){
     console.log('Server is running on port 8000');
 });
