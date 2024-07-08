3494b868c5a09431b98ffc02e74232517379b4d7
Wilfried M Majaliwa <wilfriedmajaliwa@gmail.com>
2024-07-08 20:25:08 +0300

    NODEMON

--- app.js
+++ app.js
@@ -1,2 +1,22 @@
-const moment = require('moment');
-console.log(moment().format('dddd, h:mA'));
+const http = require('node:http');
+const fs = require('node:fs');
+
+http.createServer((req, res) => {
+// executed when a request happens
+    if (req.url === '/') {
+        fs.createReadStream(`${__dirname}/index.htm`).pipe(res);
+    }
+    else if (req.url === '/api') {
+        res.writeHead(200, {'Content-Type': 'application/json'}); // when a req happens, do this, construct a response
+        const data = {
+            name: "Wilfried M. Majaliwa",
+            age: 29
+        }
+        res.end(JSON.stringify(data)); // serialize the data into JSON
+    } else {
+        res.writeHead(404, {'Content-Type': 'text/plain'});
+        res.end('Page not found');
+    }
+}).listen(8000, '127.0.0.1', null, function (){
+    console.log('Your server is running on port 8000');
+});
