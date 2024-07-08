d5ef9db27bcb04f55faca1c25fcc19ebce6dc63a
Wilfried M Majaliwa <wilfriedmajaliwa@gmail.com>
2024-07-08 19:21:53 +0300

    Routing

--- app.js
+++ app.js
@@ -3,12 +3,20 @@ const fs = require('node:fs');
 
 http.createServer((req, res) => {
 // executed when a request happens
-    res.writeHead(200, {'Content-Type': 'application/json'}); // when a req happens, do this, construct a response
-    const data = {
-        name: "Wilfried M. Majaliwa",
-        age: 29
+    if (req.url === '/') {
+        fs.createReadStream(`${__dirname}/index.htm`).pipe(res);
     }
-    res.end(JSON.stringify(data));  // serialize the data into JSON
+    else if (req.url === '/api') {
+        res.writeHead(200, {'Content-Type': 'application/json'}); // when a req happens, do this, construct a response
+        const data = {
+            name: "Wilfried M. Majaliwa",
+            age: 29
+        }
+        res.end(JSON.stringify(data)); // serialize the data into JSON
+    } else {
+        res.writeHead(404, {'Content-Type': 'text/plain'});
+        res.end('Page not found');
+    }
 }).listen(8000, '127.0.0.1', null, function (){
     console.log('Server is running on port 8000');
 });
