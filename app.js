efc08c0090d51aeaef75ee03bf9c75189e0e3976
Wilfried M Majaliwa <wilfriedmajaliwa@gmail.com>
2024-07-09 00:46:41 +0300

    Static Files and Middleware

--- app.js
+++ app.js
@@ -2,9 +2,22 @@ const express = require('express');
 
 const app = express();
 
+// when the browser issue a GET for /assets/style.css, fire up the /assets endpoint
+// and look for the specific static files after /assets in the folder public
+app.use('/assets', express.static(`${__dirname}/public`));
+
+// custom middleware
+// leave out the route, so it happens for every route
+app.use('/', function (req, res, next) {
+   console.log(`Request URL: ${req.url}`);
+   next();
+});
+
+
+
 app.get('/', (req, res) => {
     console.log(req.body);
-    res.send('<html><body><h1>Hello World!</h1></body></html>');
+    res.send('<html><head><link rel="stylesheet" href="/assets/style.css"/></head></head><body><h1>Hello World!</h1></body></html>');
 })
 
 app.get('/person/:id', (req, res) => {
